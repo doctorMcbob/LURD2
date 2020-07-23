bafd749baa216c3436c384256dcc2d90e55046fc
doctorMcbob <wootenwesley@gmail.com>
2020-07-22 22:29:37 -0700

    player

---
+++ src/player.py
@@ -0,0 +1,28 @@
+from src.locals import *
+from src.utils import get, apply_direction
+
+PLAYER_TEMPLATE = {
+    "POS": (4, 4),
+    "LVL": 0,
+    "EXP": 0,
+    "HP": 50,
+    "ATK": 5,
+    "DEF": 5,
+    "SPEED": 1,
+    "WEAPON": None,
+    "ARMOR": None,
+    "INV": [],
+    "token": "face",
+    "colors": (BLACK, DARKGREEN)
+}
+
+PLAYER = PLAYER_TEMPLATE.copy()
+
+def move(grid, d, player=PLAYER):
+    move_slot = apply_direction(player["POS"], d)
+    try:
+        if any([thing in TANGIBLES for thing in get(grid, move_slot)]):
+            return False
+    except IndexError: return False
+    player["POS"] = move_slot
+    return True
